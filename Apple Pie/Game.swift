cf9e4363023e2deb16fe543f97d5eae73d24b93d
Raquel <marinaluz371@gmail.com>
2020-03-12 16:59:59 -0700

    Finished part two of the unit. I added the total wins and total losses to the game.

---
+++ Apple Pie/Game.swift
@@ -0,0 +1,14 @@
+//
+//  Game.swift
+//  Apple Pie
+//
+//  Created by RAQUEL SANTOS on 3/12/20.
+//  Copyright © 2020 RAQUEL SANTOS. All rights reserved.
+//
+
+import Foundation
+
+struct Game {
+    var word: String
+    var incorrectMovesRemaining: Int
+}
